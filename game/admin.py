6fca8da2156ace2e7298a3aa9d2bb55304e90b07
v81d <v81d@proton.me>
2026-04-24 21:08:33 -0400

    feat(models): add card sprites model and update card fields

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Card, Rarity
+from .models import Card, CardSprite, Rarity
 
 
 @admin.register(Rarity)
@@ -8,6 +8,12 @@ class RarityAdmin(admin.ModelAdmin):
     list_display = ("name", "desperation_constant")
 
 
+class CardSpriteInline(admin.TabularInline):
+    model = CardSprite
+    extra = 1
+
+
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     list_display = ("name", "nickname", "rarity")
+    inlines = [CardSpriteInline]
--- game/models.py
+++ game/models.py
@@ -1,4 +1,13 @@
+import uuid
+
 from django.db import models
+
+
+# Pre-upload hook/procedure to randomize the filename by generating a UUID
+def card_sprite_upload_to(instance, filename):
+    ext = filename.split(".")[-1]
+    new_name = f"{uuid.uuid4()}.{ext}"
+    return f"cards/sprites/{new_name}"
 
 
 class Rarity(models.Model):
@@ -15,9 +24,22 @@ class Rarity(models.Model):
 class Card(models.Model):
     name = models.CharField(max_length=200)
     nickname = models.CharField(max_length=100)
-    spritesheet = models.ImageField(upload_to="cards/spritesheets/")
     description = models.TextField()
     rarity = models.ForeignKey(Rarity, on_delete=models.CASCADE)
+    health = models.PositiveIntegerField()
+    defense = models.PositiveIntegerField()
+    base_move_energy = models.PositiveIntegerField()
+    base_move_energy_gain = models.PositiveIntegerField()
+    desperation = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
+
+
+# This class is not exactly a model on its own, but rather an inline model that should be placed as a Card field
+class CardSprite(models.Model):
+    card = models.ForeignKey("Card", on_delete=models.CASCADE)
+    key = models.CharField(max_length=50)
+    image = models.ImageField(
+        upload_to=card_sprite_upload_to,
+    )
