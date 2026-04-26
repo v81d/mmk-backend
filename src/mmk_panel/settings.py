b125e4ab16f2627be14efe53c12e0998cbdedd4f
v81d <v81d@proton.me>
2026-04-26 19:55:12 -0400

    feat(settings): move ALLOWED_HOSTS to .env

--- src/mmk_panel/settings.py
+++ src/mmk_panel/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+    if host.strip()
+]
 
 
 # Application definition
